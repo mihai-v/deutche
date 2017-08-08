07d233e9a03a69ccd29a0589878e65320087f4ec
vasilache <mihai_vasilache@yahoo.com>
2017-08-08 11:00:44 +0300

    sdsdg

---
+++ src/main/java/ro/db/mediastore/MediastoreApplication.java
@@ -0,0 +1,12 @@
+package ro.db.mediastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediastoreApplication.class, args);
+	}
+}
